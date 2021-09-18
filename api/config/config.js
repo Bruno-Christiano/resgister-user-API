0b74cbcd558f3acf8587a51a2aec905a5b7be496
Bruno Cristiano <bruno_cris_santos@hotmail.com>
2021-09-18 18:10:21 -0400

    modficao .sequelizerc

---
+++ api/config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: '8143614452',
+    database: 'register-users',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+};
