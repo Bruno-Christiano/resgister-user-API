13c4ccabd243964b7318919938fc2ee4ab693674
Bruno Cristiano <bruno_cris_santos@hotmail.com>
2021-09-18 20:37:27 -0400

    creat .env DATABASE_URL

--- api/config/config.js
+++ api/config/config.js
@@ -16,13 +16,8 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
     dialect: 'postgres',
-    // url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
     // // dialect: 'postgres',
   },
 };
